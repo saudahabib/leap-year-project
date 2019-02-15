ec933ca4aa23314f7968ba37bacda1f1eb3fff39
saudahabib <saudababs00@gmail.com>
2019-02-15 05:57:04 +0300

    Change conditions for js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var year= prompt("Enter a year");
-if(year%4===0 && year%400===0 && year%100!==0)	{
+if(year%4===0 || year%100===0 && year%400===0)	{
 alert(year+" "+"is a leap year")
 } else {
 alert(year+" "+"is not a leap year")
